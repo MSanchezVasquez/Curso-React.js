0fdc3f0570e1d80c7bc51d0e5233e275b54906d4
MSanchezVasquez <moisesup.25@gmail.com>
2023-11-16 08:50:13 -0500

    useeffect

--- 00react-basicos/src/App.jsx
+++ 00react-basicos/src/App.jsx
@@ -12,6 +12,7 @@ import CicloDeVida from "./components/CicloDeVida";
 import AjaxApis from "./components/AjaxApis";
 import ContadorHooks from "./components/ContadorHooks";
 import ScrollHooks from "./components/ScrollHooks";
+import RelojHooks from "./components/RelojHooks";
 
 function App() {
   return (
@@ -55,6 +56,8 @@ function App() {
             <ContadorHooks />
             <hr />
             <ScrollHooks />
+            <hr />
+            <RelojHooks />
           </section>
         </header>
       </div>
