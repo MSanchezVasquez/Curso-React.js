c3d07630f6c25c3237d2b2eb41052276cefe173f
MSanchezVasquez <moisesup.25@gmail.com>
2023-11-20 12:31:32 -0500

    frameworks

---
+++ 01-react-frameworks-css/src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import Bootstrap from "./components/Bootstrap";
+
+function App() {
+  return (
+    <div>
+      <h1>Frameworks CSS con React</h1>
+      <Bootstrap />
+    </div>
+  );
+}
+
+export default App;
