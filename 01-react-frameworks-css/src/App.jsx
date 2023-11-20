1ac71ec2ffd1f46dcb95d906f4a63bb769c83951
MSanchezVasquez <moisesup.25@gmail.com>
2023-11-20 12:39:26 -0500

    replaced

--- 01-react-frameworks-css/src/App.jsx
+++ 01-react-frameworks-css/src/App.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import Bootstrap from "./components/Bootstrap";
+//import Bootstrap from "./components/Bootstrap";
+//import Bulma from "./components/Bulma";
 
 function App() {
   return (
     <div>
       <h1>Frameworks CSS con React</h1>
-      <Bootstrap />
+      {/* <Bootstrap /> */}
+      {/* <Bulma /> */}
     </div>
   );
 }
