18e4b2e66c05f47d32a947fd6f31b4e84e91b371
MSanchezVasquez <moisesup.25@gmail.com>
2023-11-21 10:00:43 -0500

    update

--- 01-react-frameworks-css/src/App.jsx
+++ 01-react-frameworks-css/src/App.jsx
@@ -2,6 +2,8 @@
 import React from "react";
 //import Bootstrap from "./components/Bootstrap";
 //import Bulma from "./components/Bulma";
+//import ReactBootstrap from "./components/ReactBootstrap";
+import MaterialUI from "./components/MaterialUI";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <h1>Frameworks CSS con React</h1>
       {/* <Bootstrap /> */}
       {/* <Bulma /> */}
+      {/* <ReactBootstrap /> */}
+      <MaterialUI />
     </div>
   );
 }
